e8b54ef1b8f731983f6bb644e54a1928d7248485
Akyl-lab <foomem@mail.ru>
2021-02-08 08:14:25 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function plus() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1); //обрежет до целого число
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1+num2;
+	//InnerHTML вывод содержимого
+	document.getElementById('out').innerHTML = result;
+}
+
+
+function minus() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1); //обрежет до целого число
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1-num2;
+	//InnerHTML вывод содержимого
+	document.getElementById('out').innerHTML = result;
+}
